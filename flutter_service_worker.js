3c3ad379b2d4b9e8b52dba925f52bd456fa8464d
TESLA2402 <>
2024-01-01 21:22:17 +0000

    :rocket: deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "4d2a074e955647542596586cbde41715",
-"index.html": "714119881cb19b892d1572662c4372a8",
-"/": "714119881cb19b892d1572662c4372a8",
+"index.html": "cab3a299485c21f288caeac8a57b3957",
+"/": "cab3a299485c21f288caeac8a57b3957",
 "assets/AssetManifest.bin": "6944e3f022ab61ad387afaaa36fd5a07",
 "assets/fonts/MaterialIcons-Regular.otf": "9567aba7ce5bad5106b305ed4832e2b1",
 "assets/assets/pdf/resume.pdf": "a2c28d243793d2514fad5cfb25642f2d",
